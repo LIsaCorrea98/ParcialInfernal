702381613fcd198de25f9e2f8476f34f16e5f9ff
Isabella Correa <pruebaslaura19@gmail.com>
2024-11-18 18:34:42 -0500

    primer commit

---
+++ src/main/java/co/edu/ufps/ParcialInfernalApplication.java
@@ -0,0 +1,13 @@
+package co.edu.ufps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParcialInfernalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParcialInfernalApplication.class, args);
+	}
+
+}
